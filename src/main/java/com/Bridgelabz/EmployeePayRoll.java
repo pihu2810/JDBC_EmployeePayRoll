d77f148a7255ec9d4643448d8108e71608f50984
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-10 19:35:20 +0530

    Connect Data Base

--- src/main/java/com/Bridgelabz/EmployeePayRoll.java
+++ src/main/java/com/Bridgelabz/EmployeePayRoll.java
@@ -1,9 +1,13 @@
 package com.Bridgelabz;
 
+
+
 public class EmployeePayRoll {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to jdbc");
+		EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
 	}
 
 }
